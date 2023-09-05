01dd77075a5272d60aaa996d3af658a969175c15
CypDasHuhn <Martinfischer533@gmail.com>
2023-09-05 09:29:03 +0200

    argument formatting

--- src/main/java/de/CypDasHuhn/BA/commands/BuildSave.java
+++ src/main/java/de/CypDasHuhn/BA/commands/BuildSave.java
@@ -6,7 +6,6 @@ import de.CypDasHuhn.BA.fileManager.RegisterStructureConfig;
 import de.CypDasHuhn.BA.message.Message;
 import de.CypDasHuhn.BA.structure.LoadStructure;
 import de.CypDasHuhn.BA.structure.SaveStructure;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
@@ -14,43 +13,36 @@ import org.bukkit.command.CommandSender;
 import static java.lang.Integer.parseInt;
 
 public class BuildSave {
-    public static void command(String[] args, CommandSender sender) {
-        // ERROR HANDLING - ARGUMENT LENGTH
-        if (args.length < 4) {
-            Message.send(sender, "to_short");
-            return;
-        }
+    public static boolean command(String[] args, CommandSender sender) {
+        try {
+            if (args.length < 4)  throw new IllegalArgumentException();
+            //region Variable Setting
+            String name = args[0];
+            World world = Command.getWorld(sender);
+            Location[] corners = {new Location(world, parseInt(args[1]), parseInt(args[2]), parseInt(args[3])), null};
+            //endregion
+            if (!LoadStructureConfig.structureRegistered(name)) {
+                if (args.length < 7) throw new IllegalArgumentException();
 
-        String name = args[0];
-        World world = Command.getWorld(sender);
-        Location cornerA = new Location(world, parseInt(args[1]), parseInt(args[2]), parseInt(args[3]));
+                RegisterStructureConfig.register(name);
 
-        if (!LoadStructureConfig.structureRegistered(name)) {
-            if (args.length < 7) {
-                Message.send(sender, "to_short");
-                return;
+                corners[1] = new Location(world, parseInt(args[4]), parseInt(args[5]), parseInt(args[6]));
+
+                if (illegalSize(corners[0], corners[1])) throw new IllegalArgumentException();
+            } else {
+                int[] size = LoadStructure.getStructureSize(name);
+                corners[1] = corners[0].clone().add(size[0], size[1], size[2]);
             }
+            SaveStructure.save(name, 0, corners[0], corners[1]);
+        } catch (IllegalArgumentException exception) {
 
-            RegisterStructureConfig.register(name);
+            return false;
+        } catch (Exception exception) {
 
-            Location cornerB = new Location(world, parseInt(args[4]), parseInt(args[5]), parseInt(args[6]));
-
-            if (illegalSize(cornerA, cornerB)) {
-                Message.send(sender, "size_big");
-                return;
-            }
-
-            SaveStructure.save(name, 0, cornerA, cornerB);
-            Bukkit.broadcastMessage("GEMACHT");
-
-        } else {
-            Bukkit.broadcastMessage("KOMISHC");
-            int[] size = LoadStructure.getStructureSize(name);
-            Location cornerB = cornerA.clone().add(size[0], size[1], size[2]);
-            SaveStructure.save(name, 0, cornerA, cornerB);
+            return false;
         }
+        return true;
     }
-
 
     public static boolean illegalSize(Location cornerA, Location cornerB) {
         return (
@@ -58,5 +50,43 @@ public class BuildSave {
             Math.abs(cornerA.getBlockY()-cornerB.getBlockY()) > 48 ||
             Math.abs(cornerA.getBlockZ()-cornerB.getBlockZ()) > 48
         );
+    }
+
+    public static String getArgumentFormat(String[] args) {
+        switch (args.length) {
+            case 7:
+                boolean isNumeric = true;
+                for (int i = 1; i < 7; i++) {
+                    if (!isNumeric(args[i])) isNumeric = false;
+                }
+
+                if (isNumeric) return "register";
+                else return null;
+            case 5:
+
+                return null;
+            case 4:
+
+                return null;
+            case 3:
+
+                return null;
+        }
+        if (args.length == 7) {
+
+        }
+        return null;
+    }
+
+    public static boolean isNumeric(String str) {
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+        for (char c : str.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
